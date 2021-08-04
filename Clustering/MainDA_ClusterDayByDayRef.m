dcbfa066666f10f15c83a43abbee19b06290e394
meritxellsaez <50885002+meritxellsaez@users.noreply.github.com>
2021-08-04 10:59:11 +0100

    Add files via upload

--- Clustering/MainDA_ClusterDayByDayRef.m
+++ Clustering/MainDA_ClusterDayByDayRef.m
@@ -1,11 +1,13 @@
 %% It clusers Data from all days and all experimental conditions in one dataset and plots the corresponding distributions and proportions
+
+%It uses the reference set for that day and experimental series to define the gmm and then cluster all experimental conditions considered 
 %% Data Loading
 
 %%path of the folder with the data. It assumes a certain structure of folders
 pathInput = '.';
 
 %%Choose one Dataset
-DataSet = '18_08_14'; %%options: '18_08_14' or '15_4_20' or '15_4_20_FOXC2' or '15_4_20_OTX2' or '18_10_25'
+DataSet = 'Dataset A'; %%options: 'Dataset A' or 'Dataset B1' or 'Dataset B2' or 'Dataset B3' or 'Dataset D'
 
 %%Options for the plots
 Reduction = 0.001; %% %of reduction for small bins
@@ -17,70 +19,36 @@ NumBins = 75; %%Number of bins in the histogram
 threshProb = 0.65; %%Minimum posterior probability of cells to cluster
 
 switch DataSet
-    case '18_08_14'
+    case 'Dataset A'
         NumIdxs = 11;
-        DataSetSave = '180814';
         NumDaysAll = 7*ones(1,NumIdxs);
         dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
-        OrderProteins=1:5;
-    case '17_3_20'
-        NumIdxs = 11;
-        DataSetSave = '170320';
-        NumDaysAll = 7*ones(1,NumIdxs);
-        dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
-        OrderProteins=[4 1 5 3 2];
-    case '17_3_20_OTX2'
+        indxsPlot = [1 3 9 5 11 7 6]; %%Colors for histogram plots
+        OrderProteins=1:5; %Reorder proteins to have coherent ordering in the plots
+    case 'Dataset B1'
         NumIdxs = 10;
-        DataSetSave = '170320OTX2';
+        NumDaysAll = 7*ones(1,NumIdxs);
+        dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
+        indxsPlot = [1 3 9 5 11 7 6 13]; %%Colors for histogram plots
+        OrderProteins=[4 1 5 3 2];
+    case 'Dataset B2'
+        NumIdxs = 10;
         NumDaysAll = 7*ones(1,NumIdxs);
         dimsClust = 1:4; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
+        indxsPlot = [1 3 9 5 11 7 6]; %%Colors for histogram plots
         OrderProteins=[4 1 3 2];
-    case '15_4_20'
-        NumIdxs = 10;
-        DataSetSave = '150420';
-        NumDaysAll = 7*ones(1,NumIdxs);
-        dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6 13];%[1 3 9 5 11 7]; %%Colors for histogram plots
-        OrderProteins=[4 1 5 3 2];
-    case '15_4_20_FOXC2'
+    case 'Dataset B3'
         NumIdxs = 1;
-        DataSetSave = '150420FOXC2';
         NumDaysAll = 7*ones(1,NumIdxs);
         dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6 13];%[1 3 9 5 11 7]; %%Colors for histogram plots
+        indxsPlot = [1 3 9 5 11 7 6 13]; %%Colors for histogram plots
         OrderProteins=[4 1 5 3 2];
-    case '15_4_20_OTX2'
-        NumIdxs = 10;
-        DataSetSave = '150420OTX2';
-        NumDaysAll = 7*ones(1,NumIdxs);
-        dimsClust = 1:4; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
-        OrderProteins=[4 1 3 2];
-    case '18_10_25'
+    case 'Dataset D'
         NumIdxs = 9;
-        DataSetSave = '181025';
         NumDaysAll = [3,3,3,2,2,3,2,2,2];
         dimsClust = 1:5; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
+        indxsPlot = [1 3 9 5 11 7 6]; %%Colors for histogram plots
         OrderProteins=1:5;
-    case '12_5_20_OTX2'
-        NumIdxs = 19;
-        DataSetSave = '120520';
-        NumDaysAll = 7*ones(1,NumIdxs);
-        dimsClust = 1:4; %%Change to cluster using only a subset of the dimensions
-        %indxsPlot = [1 5 3 9 7 11];
-        indxsPlot = [1 3 9 5 11 7 6];%[1 3 9 5 11 7]; %%Colors for histogram plots
-        OrderProteins=[1,2,4,3];
 end
 
 
@@ -89,7 +57,7 @@ NameData = cell(1,NumIdxs);
 NameDays = cell(1,NumIdxs);
 
 for DataIdx = 1:NumIdxs %%Load Data from all experimental conditions
-    [Data{DataIdx}, Proteins, NameData{DataIdx}, NameDays{DataIdx}, DataSetSave, SaveName] =...
+    [Data{DataIdx}, Proteins, NameData{DataIdx}, NameDays{DataIdx}, DataSetSave] =...
         DA_getData(DataSet, DataIdx, pathInput);
 end
 
@@ -106,7 +74,7 @@ NumSamples = size(Data{1}{1},1); %%We are assuming the number of samples is cons
 gmfitaux=[];
 numclustaux=0;
 
-if strcmp(DataSet, '18_08_14')
+if strcmp(DataSet, 'Dataset A')
     DataIdxsAll={1,[1,3],[1,3],[1,3,6],[1,3,6],[1,3,6],[1,3,6]}; %Experimental conditions to define GMM each day. Reference set
     NumClustAll=[1,3,4,6,6,6,5]; %Number of clusters each day
     %InitmuAll contains the mu used to initialise the gmm fitting in order
@@ -118,7 +86,7 @@ if strcmp(DataSet, '18_08_14')
         1.e4*[2.3261    0.7451    1.5068    0.3375    0.9711; 0.7238    0.3254    1.3005    3.5918    2.1064; 0.5782    0.8986    2.9840    0.8401    0.9549; 0.5780    0.2670    0.8282    4.4704    1.3627; 0.5071    0.2635    0.7301    1.7883    2.0980; 0.2 0.2 0.2 0.2 0.2],...
         1.e4*[0.5431    0.2685    0.7876    0.2622    0.8531; 0.4899    0.4752    2.2509    0.9558    0.9039; 0.4817    0.2181    0.5982    3.0215    1.5508; 0.7954    0.3091    1.1974    4.1025    2.2023; 1.8517    0.3901    1.0812    0.2761    0.8592]};
     indxsPlotLandsAll={2,[2,9,8],[15,2,9,9],[9,15,9,6,13,13],[15,9,15,13,6,13],[15,13,9,6,13,15],[15,9,13,6,15]}; %Position in the table of proportions of the corresponding clustered cells corresponding to the assigned cell type
-elseif strcmp(DataSet, '15_4_20_FOXC2')
+elseif strcmp(DataSet, 'Dataset B3')
     DataIdxsAll={1,1,1,1,1,1,1};
     NumClustAll=[1,2,1,3,2,3,3];
     InitmuAll={1.e3*[1,1,1,1,1], 1.e3*[2.8083    2.5262    0.6219    2.6572    2.0181; 3.6546    3.3986    0.8964    5.2877    2.6307],...
@@ -128,8 +96,7 @@ elseif strcmp(DataSet, '15_4_20_FOXC2')
         1.e4*[0.2723    1.1541    0.0421    0.0639    0.2531; 0.7543    0.7206    0.1618    0.0856    0.2194; 0.2856    0.2358    0.4073    0.2217    0.2013],...
         1.e3*[2.7076    8.7882    0.3978    0.6138    2.0714; 6.9597    8.7416    1.3918    1.0663    2.1853; 2.8454    2.3847    3.0271    3.4474    2.5288]};
     indxsPlotLandsAll={2,[9,2],[9],[9,15,9],[15,9],[10,15,9],[10,9,15]};
-    Proteins{1}='FOXC2';
-elseif strcmp(DataSet, '15_4_20_OTX2')
+elseif strcmp(DataSet, 'Dataset B2')
     DataIdxsAll={1,[1,3],[1,3],[1,3,4],[1,3,4],[1,3,4],[1,3,4]};
     NumClustAll=[1,2,2,6,6,5,6];
     InitmuAll={1.e3*[1,1,1,1], 1.e4*[1.8310    1.5959    1.3416    0.5960; 1.0100    1.2675    0.6597    0.4379],...
@@ -139,7 +106,7 @@ elseif strcmp(DataSet, '15_4_20_OTX2')
         1.e4*[0.9434    0.9342    1.9917    1.2715; 0.2095    0.6767    0.1258    0.4860; 1.9872    1.3856    2.0897    1.9068; 0.3257    0.7416    0.9901    2.4566; 0.2302    2.9638    0.3201    0.5817],...
         1.e4*[0.1867    0.6070    0.1115    0.4703; 0.6084    0.9031    1.3768    1.7654; 0.2038    2.6359    0.4979    0.6577; 2.2345    1.1825    2.1698    1.6172; 0.3510    0.9645    0.2095    0.9158; 0.9916    1.5587    1.6668    2.8455]};
     indxsPlotLandsAll={2,[2,8],[2,9],[9,6,6,9,9,13],[13,9,6,15,9,6],[6,15,6,13,9],[15,13,9,6,9,13]};
-elseif strcmp(DataSet, '15_4_20')
+elseif strcmp(DataSet, 'Dataset B1')
     DataIdxsAll={1,[1,3],[1,3],[1,3,4],[1,3,4],[1,3,4],[1,3,4]};
     NumClustAll=[1,3,5,7,6,7,4];
     InitmuAll={1.e3*[1,1,1,1,1], 1.e4*[0.4194    0.4099    0.2242    1.0102    0.3385; 0.3037    0.3004    0.1677    0.5416    0.2485; 0.3902    0.7431    0.2624    0.4160    0.3257],...
@@ -149,37 +116,24 @@ elseif strcmp(DataSet, '15_4_20')
         1.e4*[0.3076    0.9128    0.0673    0.8314    2.0661; 0.3029    0.4307    0.8766    0.2716    0.3304; 0.3268    0.5679    0.0852    1.3070    0.8293; 0.2969    0.3831    0.0861    0.0731    0.3336; 0.4794    1.0879    0.9573    0.2299    0.4276; 0.8219    0.5747    0.2795    0.0727    0.2582; 0.4430    0.8008    0.1413    1.5442    1.3713],...
         1.e4*[0.2821    0.4261    0.0661    0.0673    0.3461; 0.3471    0.7452    0.0840    1.0826    1.3456; 0.3033    0.5789    0.4579    0.4040    0.4418; 0.5427    0.4741    0.1836    0.0780    0.2738]};
     indxsPlotLandsAll={2,[2,8,9],[9,2,9,9,2],[9,6,9,13,9,6,15],[15,9,13,9,15,6],[13,9,6,15,9,15,6],[15,13,9,15]};
-elseif strcmp(DataSet, '18_10_25')
+elseif strcmp(DataSet, 'Dataset D')
     DataIdxsAll={1,[1,3],[1,3]};
     NumClustAll=[2,4,4];
     InitmuAll={ 1.e+3*[1.5844    1.0719    3.6462    0.4538    1.5928; 1.3073    0.2214    2.8132    0.3436    1.4554],...
         1.e+3*[1.4144    0.7929    5.4308    0.4541    1.9722; 1.1200    0.1559    1.3610    1.0863    7.5997; 3.8337    0.5371    3.0780    0.1682    1.5279; 1.1642    0.2080    1.02    0.2040    1.7089],...
         1.e3* [1.2960    0.1589    1.0644    1.5883    8.1280; 3.8533    0.2701    2.1362    0.1775    1.7563; 1.5227    0.4152    4.8959    0.5899    2.7694; 1.3999    0.1802    1.2072    0.1724    1.9519] };
     indxsPlotLandsAll={[9, 9],[9, 13, 15, 15],[13, 15, 9, 15] };
-elseif strcmp(DataSet, '12_5_20_OTX2')
-    DataIdxsAll={1,[1,3],[1,3], [1,3,4], [1,3,4], [1,3,4], [1,3,4]};
-    NumClustAll=[1, 3, 2, 4, 4,5,5];
-    InitmuAll={1.e+3 *[7.9491    2.3310    4.8007    0.8788],...
-        1.e+4*[0.7637    0.3679    0.2253    0.1145; 0.5209    0.2480    0.2760    0.0811; 1.3209    0.3338    0.5691    0.1204],...
-        1.e+4*[0.3107    0.3895    0.1292    0.0989; 1.0076    0.2552    0.4457    0.1054],...
-        1.e+4*[0.2911    0.3429    0.0956    0.1339; 0.5624    0.3315    0.3857    0.2246; 0.3096    0.7715    0.1789    0.1592; 1.7349    0.4434    0.9242    0.2422],...
-        1.e+4*[0.6088    0.2669    0.3807    0.4306; 1.8170    0.2645    0.9130    0.2721; 0.2096    0.2235    0.0775    0.1130; 0.2127    0.6591    0.1318    0.1522],...
-        1.e+4*[0.1984    0.1893    0.0763    0.1020; 0.3998    0.1934    0.6783    0.6430; 1.0458    0.2188    1.0225    0.2644; 1.8886    0.3098    1.1779    0.4068; 0.1829    0.7566    0.1925    0.1473],...
-        1.e+4*[0.2208    0.2046    0.0762    0.1306; 0.2076    0.9088    0.3245    0.2276; 0.8521    0.1907    0.9345    0.2392; 1.5791    0.2588    1.1210    0.3798; 0.3108    0.1885    0.6302    0.6635]};
-    indxsPlotLandsAll={2,[9,8,2],[9,2],[15,13,9,6],[13,6,15,9],[15,13,6,6,9],[15,9,6,6,13]};
 end
 gmfitAll=cell(1,max(NumDaysAll));
 
-%% Perform clustering day by day
-
+%% Define gmm from reference set day by day
 
 for DaysToUse=1:max(NumDaysAll)
     
     DataIdxs=DataIdxsAll{DaysToUse};
     NumDaysUsed=numel(DaysToUse);
     
-    DataAnalise = zeros(numel(DataIdxs)*NumDaysUsed*NumSamples, NumProt+1); %Put all Data in a matrix
-    
+    DataAnalise = zeros(numel(DataIdxs)*NumDaysUsed*NumSamples, NumProt+1); %Put all Reference Data in a matrix
     
     for i = 1:numel(DataIdxs)
        for j=1:NumDaysUsed
@@ -222,15 +176,13 @@ for DaysToUse=1:max(NumDaysAll)
     DA_PlotCombined1DHist_gmfitOrigin(Data2, dimsClust, 1:NumClust, Reduction, clusterX, Proteins(OrderProteins), NumBins, saving, [], [NameDays{1}{DaysToUse}], '', DataSetSave, indxsPlot, gmfit, gmfitaux, numclustaux);
     DA_PlotCombined2DHist(Data2, smooth, 1:NumProt, [1,2,3], Reduction,  clusterX, Proteins(OrderProteins), NumBins, saving, [], [NameDays{1}{DaysToUse}], [], DataSetSave, indxsPlot);
     
-    gmfitAll{DaysToUse}=gmfit; %save obtained gmm
+    gmfitAll{DaysToUse}=gmfit; %save obtained gmm to perform clustering later
 end
 
 %%  Use the global distribution to cluster each experimental condition for each day and plot the resulting proportion tables
 
 ClusteredDataWithThd = zeros(7, NumDays, 7);
 auxIdx=1;
-
-
 ClusteredDataAll = cell(11,7);
 ClusteredDataUndTrans = cell(11,7);
 
@@ -264,12 +216,12 @@ for DataIdx = 1:NumIdxs %%Change to plot only some experimental conditions
         indxsPlotLands=indxsPlotLandsAll{Day};
         
         %%Specific proporties of cell ientity assignment
-        if strcmp(DataSet, '15_4_20')
+        if strcmp(DataSet, 'Dataset B1')
             if DataIdx==1 || DataIdx==8
                 aux=find(indxsPlotLands==8);
                 indxsPlotLands(aux) = 2*ones(size(aux));
             end
-        elseif strcmp(DataSet, '15_4_20_OTX2')
+        elseif strcmp(DataSet, 'Dataset B2')
             if DataIdx==1 || DataIdx==8
                 aux=find(indxsPlotLands==8);
                 indxsPlotLands(aux) = 2*ones(size(aux));
@@ -284,7 +236,7 @@ for DataIdx = 1:NumIdxs %%Change to plot only some experimental conditions
         Landscape(5)=sum(P<=threshProb)/size(DataX,1);
         
         %%Specific proporties of cell ientity assignment
-        if strcmp(DataSet, '18_08_14')
+        if strcmp(DataSet, 'Dataset A')
             if Day==5
                 auxLands = Landscape(6);
             elseif Day>5
@@ -293,7 +245,7 @@ for DataIdx = 1:NumIdxs %%Change to plot only some experimental conditions
                 Landscape(13) = max(auxN-auxLands,0);
                 
             end
-        elseif strcmp(DataSet, '15_4_20')
+        elseif strcmp(DataSet, 'Dataset B1')
             if  Day==5
                 auxLands = Landscape(6);
             elseif  Day>5
@@ -305,7 +257,7 @@ for DataIdx = 1:NumIdxs %%Change to plot only some experimental conditions
                 Landscape(8)=Landscape(8)+Landscape(2);
                 Landscape(2)=0;
             end
-        elseif strcmp(DataSet, '15_4_20_OTX2')
+        elseif strcmp(DataSet, 'Dataset B2')
             if Day==5
                 auxLands = Landscape(6);
             elseif Day>5
@@ -317,7 +269,7 @@ for DataIdx = 1:NumIdxs %%Change to plot only some experimental conditions
                 Landscape(2)=0;
             end
         end
-        LandsToMatrix = [2, 8, 6, 13, 15, 9, 5 ];
+        LandsToMatrix = [2, 8, 6, 13, 15, 9, 5 ]; %Reorder states from table disposition to a vector
         for i=1:7
             ClusteredDataWithThd(auxIdx, Day, i) = Landscape(LandsToMatrix(i));
         end
---
+++ Clustering/ReadMe.txt
@@ -0,0 +1,13 @@
+MainDA_ClusterDayByDayRef.m performs the clustering of the experiemntal FACS data considered in 
+Saez, Blassberg, Camacho-Aguilar et al. 2021. Cell systems.
+
+The names of the datasets are as detailed in the supplementary table T1 included as supplementary 
+information with the paper.
+Dataset A is the Initial experimental series, Dataset B is the test experimental series and 
+Dataset D is the Prediction experimental series relating to the bifurcation of CE. 
+No clustering was performed for Dataset C, which relates to the predictions about AN stability.
+
+The code shows plots with the distributions obtained from the pooled reference set of conditions 
+(as defined in the paper) and tables with the proportions allocated to each cell state, at each 
+time point for each experimental condition. 
+The summary of all the proportions is included as supplementary table T2 with the paper.
